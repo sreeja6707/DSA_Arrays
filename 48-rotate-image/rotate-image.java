1a192c0b83a67373d9a9f8142154231faf02246b
SREEJA BODASU <153908355+sreeja6707@users.noreply.github.com>
2025-09-04 21:04:27 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (41.18%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,16 +1,20 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n=matrix.length;
-        int ans[][]=new int[n][n];
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                ans[j][(n-1)-i]=matrix[i][j];
+       // int ans[][]=new int[n][n];
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
         for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                matrix[i][j]=ans[i][j];
-            }
+            for(int j=0;j<n/2;j++){
+            int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - 1 - j];
+                matrix[i][n - 1 - j] = temp;
+        }
         }
     }
 }
