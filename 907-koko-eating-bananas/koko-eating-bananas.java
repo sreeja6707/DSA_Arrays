a207a016ee398185613f0cba9423787232274128
SREEJA BODASU <153908355+sreeja6707@users.noreply.github.com>
2026-02-06 20:10:29 +0530

    Time: 27 ms (14.13%) | Memory: 48.8 MB (8.69%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.java
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -1,9 +1,34 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
+    public int findMax(int[] piles){
+        int maxi=Integer.MIN_VALUE;
+        for(int i=0;i<piles.length;i++){
+            maxi=Math.max(piles[i],maxi);
+        }
+        return maxi;
+    }
+    public int ceilt(int[] piles,int hour){
+        int totalH=0;
+        int n=piles.length;
+        for(int i=0;i<n;i++){
+            totalH+=Math.ceil((double)(piles[i])/(double)(hour));
+        }
+        return totalH;
+    }
+    public int eat(int[] piles,int h){
+        int maxi=findMax(piles);
+        for(int i=0;i<=maxi;i++){
+            int r=ceilt(piles,h);
+            if(r<=h){
+                return i;
+            }
+          }
+          return maxi;
+        }
+        public int minEatingSpeed(int[] piles, int h) {
         int low=1,high=findMax(piles);
         while(low<=high){
             int mid=(low+high)/2;
-            int totalH=calTotalHrs(piles,mid);
+            int totalH=ceilt(piles,mid);
             if(totalH<=h){
                 high=mid-1;
             }
@@ -11,22 +36,7 @@ class Solution {
                 low=mid+1;
             }
         }
-        return low;
-    }
-    public static int findMax(int[] piles){
-        int n=piles.length;
-        int maxi=Integer.MIN_VALUE;
-        for(int i=0;i<n;i++){
-            maxi=Math.max(maxi,piles[i]);
-        }
-        return maxi;
-    }
-    public static int calTotalHrs(int[] piles,int h){
-        int totalH=0;
-        int n=piles.length;
-        for(int i=0;i<n;i++){
-            totalH+=Math.ceil((double)(piles[i])/(double)(h));
-        }
-        return totalH;
+    return low;
+
     }
 }
