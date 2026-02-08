208661593b73b1f12d330d79069aef0f93cd32e2
SREEJA BODASU <153908355+sreeja6707@users.noreply.github.com>
2026-02-08 10:07:20 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (67.13%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,38 +1,51 @@
-
 class Solution {
-    private int countPartitions(int[] nums, long maxSum) {
-        int partitions = 1;
-        long sum = 0;
-        for (int num : nums) {
-            if (sum + num <= maxSum) {
-                sum += num;
-            } else {
-                partitions++;
-                sum = num;
-            }
-        }
-        return partitions;
-    }
-
     public int splitArray(int[] nums, int k) {
-        long low = 0, high = 0;
-        for (int num : nums) {
-            low = Math.max(low, num);
-            high += num;
+        int n=nums.length;
+        if (k > n)
+            return -1;
+        int low=findMax(nums);
+        int high=sumN(nums);
+        while(low<=high){
+            int mid=(low+high)/2;
+                if(maxpages(nums,mid)>k){
+                    low=mid+1;
+                }
+                else{
+                    high=mid-1;
+                }
         }
-
-        long ans = high;
-        while (low <= high) {
-            long mid = low + (high - low) / 2;
-            int needed = countPartitions(nums, mid);
-            if (needed > k) {
-                low = mid + 1;
-            } else {
-                ans = mid;
-                high = mid - 1;
+        return low;
+    }
+    public int findMax(int[] arr){
+        int n=arr.length;
+        int maxi=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            if(arr[i]>maxi){
+                maxi=arr[i];
             }
         }
-        return (int) ans;
+        return maxi;
+    }
+    public int sumN(int[] arr){
+        int sum=0;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+    public int maxpages(int[] arr, int k){
+        int stud=1;int pages=0;
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(pages+arr[i]<=k){
+                pages+=arr[i];
+            }
+            else{
+                stud++;
+                pages=arr[i];
+            }
+        }
+        return stud;
     }
 }
-
